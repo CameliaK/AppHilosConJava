bf71dfe9efeb36d66b8bb8dc8c7c0e5739ea9253
CameliaK <133041902+CameliaK@users.noreply.github.com>
2024-03-15 02:28:16 -0600

    AppConHilosJava

---
+++ Programa1.java
@@ -0,0 +1,15 @@
+public class Programa1 implements Runnable{
+
+    @Override
+    public void run(){
+        System.out.println(Thread.currentThread().getName() + " subproceso corriendo...");
+    }
+
+    public static void main(String[] args){
+        Thread hilo = new Thread(new Programa1(), "Hilo1");
+        hilo.start();
+        System.out.println("Este codigo está fuera del hilo");
+    }
+
+
+}
